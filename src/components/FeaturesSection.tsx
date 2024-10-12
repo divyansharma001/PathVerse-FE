5ebc49749ba90848b5d9e795a5136f7712fe8889
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-12 23:00:42 +0530

    Refactor FeaturesSection component to update text content

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -45,7 +45,7 @@ export function FeaturesSection() {
     <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto">
       <div className="px-8">
         <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
-          Packed with thousands of features
+          Packed with tons of features
         </h4>
 
         <p className="text-sm lg:text-base  max-w-2xl  my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
