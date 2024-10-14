8df4b0386b70a9bc6419f05fd2655bba417271b8
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-14 23:43:08 +0530

    Refactor sidebar component and add sidebar to dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { SidebarDash } from '@/components/SidebarDash'
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SidebarDash/>
+    </div>
+  )
+}
+
+export default page
