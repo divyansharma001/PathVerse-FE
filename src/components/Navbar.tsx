5844496d92cf1c1f335b1601977ca6ae2dde863c
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-11-08 23:42:28 +0530

    Fix formatting in Navbar component by removing extra space in icon class

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <RiStockLine className="h-8 w-8 text-primary" />
+            <RiStockLine className="h-8 w-8 text-primary"/>
             <HoverCard>
               <HoverCardTrigger asChild>
                 <Link href="/">
